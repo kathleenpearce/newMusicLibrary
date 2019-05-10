3b65ded4ea473cbc19604c67fe9ed4b9672c1d8b
Kathleen pearce <kathleenpearce@live.com>
2019-05-10 19:39:13 +0000

    new music library assingment

---
+++ NewLibrary.js
@@ -0,0 +1,37 @@
+var Library = function(name, creator) {
+  this.name: "name",
+  this.reator: "creator",
+  this.playlists: []
+
+}
+
+
+Library.prototype.playlist = function(playlistName) {
+  this.playlistName: [playlistName],
+  this.tracks: tracks
+}
+
+Library.prototype.tracks = functions(title, rating) {
+  this.title = "title",
+  this.rating = 0,
+  this.length = 0
+
+}
+
+
+Library.prototype.overallRating = function(averageRating) {
+    for (let i = 0; i < tracks; i++) {
+     let averageRating = tracks.rating + i {
+        return  averageRating;
+      }
+    }
+  }
+
+
+Library.prototype.totalDuration = function(totalDuration) {
+    for (let i = 0; i < tracks.length; i++) {
+     let totalDuration = tracks.length + i {
+        return  totalDuration;
+      }
+    }
+  }
